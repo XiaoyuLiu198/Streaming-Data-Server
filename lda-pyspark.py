ca54350ee7a11f59468904db48f606f17a5fd59f
XiaoyuLiu198 <65391883+XiaoyuLiu198@users.noreply.github.com>
2021-07-10 19:22:59 -0500

    Update lda-pyspark.py

--- lda-pyspark.py
+++ lda-pyspark.py
@@ -21,7 +21,7 @@ spark._jsc.hadoopConfiguration().set("com.amazonaws.services.s3.enableV4", "true
 spark._jsc.hadoopConfiguration().set("fs.s3a.aws.credentials.provider","org.apache.hadoop.fs.s3a.BasicAWSCredentialsProvider")
 spark._jsc.hadoopConfiguration().set("fs.s3a.endpoint", "us-east-2.amazonaws.com")
 get_last_modified = lambda obj: int(obj['LastModified'].strftime('%s'))
-
+file_location_http="xxx"
 s3 = boto3.client('s3')
 objs = s3.list_objects_v2(Bucket='twitter')['Contents']
 last_added = [obj['Key'] for obj in sorted(objs, key=get_last_modified)][0]
@@ -63,7 +63,7 @@ cv_model = cv.fit(tokens_df)
 vectorized_tokens = cv_model.transform(tokens_df)
 
 from pyspark.ml.clustering import LDA
-num_topics = 3
+num_topics = 10
 lda = LDA(k=num_topics, maxIter=10)
 model = lda.fit(vectorized_tokens)
 ll = model.logLikelihood(vectorized_tokens)
@@ -96,4 +96,37 @@ def format_data_to_pyldavis(df_filtered, count_vectorizer, transformed, lda_mode
 data = format_data_to_pyldavis(tokens_df, cv_model, transformed, model)
 py_lda_prepared_data = pyLDAvis.prepare(**data)
 pyLDAvis.display(py_lda_prepared_data)
+pyLDAvis.save_html(vis,file_location_http+'lda.html')
 
+# 2. Wordcloud of Top N words in each topic
+from matplotlib import pyplot as plt
+from wordcloud import WordCloud, STOPWORDS
+import matplotlib.colors as mcolors
+
+cols = [color for name, color in mcolors.TABLEAU_COLORS.items()]  # more colors: 'mcolors.XKCD_COLORS'
+
+cloud = WordCloud(stopwords=en_stops,
+                  background_color='white',
+                  width=2500,
+                  height=1800,
+                  max_words=10,
+                  colormap='tab10',
+                  color_func=lambda *args, **kwargs: cols[i],
+                  prefer_horizontal=1.0)
+
+fig, axes = plt.subplots(2, 5, figsize=(10,10), sharex=True, sharey=True)
+
+for i, ax in enumerate(axes.flatten()):
+    fig.add_subplot(ax)
+    topic_words = dict(topics[i][1])
+    cloud.generate_from_frequencies(topics_words, max_font_size=300)
+    plt.gca().imshow(cloud)
+    plt.gca().set_title('Topic ' + str(i), fontdict=dict(size=16))
+    plt.gca().axis('off')
+
+
+plt.subplots_adjust(wspace=0, hspace=0)
+plt.axis('off')
+plt.margins(x=0, y=0)
+plt.tight_layout()
+plt.savefig(file_location_http+'fig.png')
